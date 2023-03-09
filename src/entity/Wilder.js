24b509907781a72cd1129785d0480c32d3809553
Madeline Thomas <maddie.thms@gmail.com>
2023-03-09 17:25:42 +0100

    change entity

--- src/entity/Wilder.js
+++ src/entity/Wilder.js
@@ -1,24 +1,30 @@
-
-
 const EntitySchema = require("typeorm").EntitySchema;
 
 module.exports = new EntitySchema({
-    name:"Wilder",
-    columns:{
-        id:{
-            primary:true,
-            type: "int",
-            generated: true,
-        },
-        name: {
-            type: "text",
-        },
-        city: {
-            type: "text",
-        },
-        email: {
-            type: "text",
-            unique: true
-        }
+  name: "Wilder",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
     },
+    name: {
+      type: "text",
+    },
+    city: {
+      type: "text",
+    },
+    email: {
+      type: "text",
+      unique: true,
+    },
+  },
+  relations: {
+    skills: {
+      type: "many-to-many",
+      target: "Skill",
+      joinTable: true,
+      eager: true,
+    },
+  },
 });
